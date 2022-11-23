e364b65676de924fd4ab4438c57f8f7a571229ef
clelioamc <clelioalves9191@gmail.com>
2022-11-23 17:43:40 -0300

    variáveis

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
+/*esse pedaço de texto
+não será 
+executado 
+ */
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       //este é um comentário
+       String x = "Olá Mundo" ; 
+       int i = 1;
+       final double e = 2.72f;
+       float e = 2.72f;
+       char c = 'a';
+       boolean b = true;
+       i = 2;
+
+
+    System.out.println(i);  //Será impresso olá mundo
     }
+
 }
