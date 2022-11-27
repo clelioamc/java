42546d3cb2cd35969fbff256a6ccdb271e2384e3
clelioamc <clelioalves9191@gmail.com>
2022-11-27 01:18:16 -0300

    CAMC Project

--- src/App.java
+++ src/App.java
@@ -6,16 +6,23 @@ executado
 public class App {
     public static void main(String[] args) throws Exception {
        //este é um comentário
-       String x = "Olá Mundo" ; 
-       int i = 1;
-       final double e = 2.72f;
-       float e = 2.72f;
-       char c = 'a';
-       boolean b = true;
-       i = 2;
+       char[] texto_c={'c','u','r','s','o',' ','d','e',' ','j','a','v','a'};
+      String texto_s=new String("CAMC Project");
+      
+       byte numeroPequeno = 127;
+       short numeroShort = 32767;
+       int numeroInteiro = 214748364;
+        
+       String s1=new String();
+       String s2=new String(texto_s);
+       String s3=new String(texto_c);
+       String s4=new String(texto_c,0,13);
 
-
-    System.out.println(i);  //Será impresso olá mundo
+       System.out.println(s2);
+    System.out.println(numeroPequeno);  
+    System.out.println(numeroShort);
+    System.out.println( numeroInteiro);
+    
     }
 
 }
