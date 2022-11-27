b48aa5a412c6ed9bed3b29c7879bb7e5408415ee
clelioamc <clelioalves9191@gmail.com>
2022-11-27 01:44:20 -0300

    enter \n

--- src/App.java
+++ src/App.java
@@ -6,19 +6,22 @@ executado
 public class App {
     public static void main(String[] args) throws Exception {
        //este é um comentário
-       char[] texto_c={'c','u','r','s','o',' ','d','e',' ','j','a','v','a'};
-      String texto_s=new String("CAMC Project");
-      
-       byte numeroPequeno = 127;
-       short numeroShort = 32767;
-       int numeroInteiro = 214748364;
+       
+    char[] texto_c={'C','u','r','s','o',' ','d','e',' ','J','a','v','a'};
+    String texto_s=new String("CAMC Project");
+    String primeiroNome = "\n \"Clélio\"";
+    byte numeroPequeno = 127;
+    short numeroShort = 32767;
+    int numeroInteiro = 214748364;
         
-       String s1=new String();
-       String s2=new String(texto_s);
-       String s3=new String(texto_c);
-       String s4=new String(texto_c,0,13);
-
-       System.out.println(s2);
+    String s1=new String();
+    String s2=new String(texto_s);
+    String s3=new String(texto_c);
+    String s4=new String(texto_c,0,13);
+   
+    System.out.println(texto_c);
+    System.out.println(primeiroNome);  
+    System.out.println(s2);
     System.out.println(numeroPequeno);  
     System.out.println(numeroShort);
     System.out.println( numeroInteiro);
